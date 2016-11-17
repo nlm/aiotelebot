6a0b209f73fbab127229e03b1fae6e73c2636ad5
Nicolas Limage <github@xephon.org>
2016-11-17 14:32:31 +0100

    removed unused file abc

--- aiotelebot/abc.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-class UpdateDispatcher(metaclass=ABCMeta):
-    pass
-
-class UpdateHandler(metaclass=ABCMeta):
-    @abstractmethod
-    def handle_update(self, update):
-        pass
